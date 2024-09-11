7ca2892a07d4471f4c85dac6593b14738caaef1e
Nick Melms <nick@craftedagency.com>
2024-09-10 21:33:23 -0400

    render map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
-import Image from "next/image";
+"use client";
+import mapboxgl from "mapbox-gl";
+import React, { useRef, useEffect, useState } from "react";
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_NOMAD_SECRET_KEY as string;
 
 export default function Home() {
-  function add(num: number): number {
-    return num;
-  }
-  return <div className=""></div>;
+  console.log(process.env.NEXT_PUBLIC_NOMAD_SECRET_KEY);
+  const mapContainer = useRef<HTMLDivElement | null>(null);
+  const map = useRef<mapboxgl.Map | null>(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return;
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current as HTMLDivElement,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
+
+  return (
+    <div className="">
+      <div ref={mapContainer} className="map-container h-dvh" />
+    </div>
+  );
 }
