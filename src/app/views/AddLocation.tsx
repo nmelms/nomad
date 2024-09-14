e63a6aea35b9e94f36ec68284a77fa0cf6008c3e
Nick Melms <nick@craftedagency.com>
2024-09-14 15:03:39 -0400

    create basic from with shadcn and zod

--- src/app/views/AddLocation.tsx
+++ src/app/views/AddLocation.tsx
@@ -1,11 +1,64 @@
 import React from "react";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const fromSchema = z.object({
+  emailAddress: z.string().email(),
+});
+
+const handleSubmit = () => {
+  console.log("submit");
+};
 
 const AddLocation = () => {
+  const form = useForm<z.infer<typeof fromSchema>>({
+    resolver: zodResolver(fromSchema),
+    defaultValues: {
+      emailAddress: "",
+    },
+  });
   return (
-    <div id="add-location-view" className="bg-white z-10 active-view">
+    <div
+      id="add-location-view"
+      className="bg-white z-10 active-view text-black"
+    >
       <h1>Add A location</h1>
       {/* TODO: Create a add location route */}
-      <form action=""></form>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)}>
+          <FormField
+            name="emailAddress"
+            control={form.control}
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel>Email Address</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="email address"
+                      type="email"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          ></FormField>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
   );
 };
