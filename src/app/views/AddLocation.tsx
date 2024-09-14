acf96db4bf60ecce0958788a20d0cd60128a6ca7
Nick Melms <nick@craftedagency.com>
2024-09-14 14:33:33 -0400

    add shadcn forms

--- src/app/views/AddLocation.tsx
+++ src/app/views/AddLocation.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const AddLocation = () => {
   return (
     <div id="add-location-view" className="bg-white z-10 active-view">
-      AddLocation
+      <h1>Add A location</h1>
+      {/* TODO: Create a add location route */}
+      <form action=""></form>
     </div>
   );
 };
