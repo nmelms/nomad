0ce7ac38ff0e71e6a55bbe9f51b9bcf821cadb56
Nick Melms <nick@craftedagency.com>
2024-09-16 13:16:56 -0400

    refactor for step 1

--- src/app/views/AddLocation.tsx
+++ src/app/views/AddLocation.tsx
@@ -1,36 +1,7 @@
 import React from "react";
 import * as z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect } from "react";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCampground,
-  faCity,
-  faMap,
-  faMountain,
-  faCrosshairs,
-  faCross,
-} from "@fortawesome/free-solid-svg-icons";
-import supabase from "../../lib/supabaseClient";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import Step1 from "../views/add-location-steps/Step1";
 
 // TODO: probably want to fix this import alias
 import { AddLocationProps, LocationData } from "@/../types";
@@ -73,176 +44,19 @@ const AddLocation: React.FC<AddLocationProps> = ({
       id="add-location-view"
       className="bg-white z-10 active-view text-black"
     >
-      <h1>Add A location</h1>
+      <div className="flex justify-center pt-4">
+        <h1>Add A location</h1>
+      </div>
+
       {/* TODO: Create a add location route */}
-      <Form {...form}>
-        <form
-          className="grid gap-5 grid-cols-2 p-5"
-          onSubmit={form.handleSubmit(handleSubmit)}
-        >
-          {/* location Name */}
-          <FormField
-            name="locationName"
-            control={form.control}
-            render={({ field }) => {
-              return (
-                <FormItem className="col-span-2">
-                  <FormLabel>Location Name</FormLabel>
-                  <FormControl>
-                    <Input
-                      defaultValue={field.value}
-                      placeholder="Location Name"
-                      type="text"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          ></FormField>
-          {/* longitude */}
-          <FormField
-            name="longitude"
-            control={form.control}
-            render={({ field }) => {
-              return (
-                <FormItem className="col-span-1">
-                  <FormLabel>Lng</FormLabel>
-                  <FormControl>
-                    <Input
-                      id="lngField"
-                      placeholder="lng"
-                      type="text"
-                      {...field}
-                      onChange={(event) => field.onChange(+event.target.value)}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          ></FormField>
-          {/* latitude */}
-          <FormField
-            name="latitude"
-            control={form.control}
-            render={({ field }) => {
-              return (
-                <FormItem className="col-span-1">
-                  <FormLabel>Lat</FormLabel>
-                  <FormControl>
-                    <Input
-                      id="latField"
-                      placeholder="lat"
-                      type="text"
-                      style={{ appearance: "none" }}
-                      {...field}
-                      onChange={(event) => field.onChange(+event.target.value)}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          ></FormField>
-          {/* find on map */}
-          <Button
-            type="button"
-            onClick={handleFindOnMap}
-            className="col-span-2"
-          >
-            Find Location on Map
-            <FontAwesomeIcon className="ps-5" icon={faMap} />
-          </Button>
-          {/* use current location */}
-          <Button
-            type="button"
-            onClick={handleUseLocation}
-            className="col-span-2 "
-          >
-            <span className="pe-5">Use Current Location</span>
-            <FontAwesomeIcon
-              className={`${gettingLocation ? "spin" : ""}`}
-              icon={faCrosshairs}
-            />
-          </Button>
-          {/* category */}
-          <FormField
-            control={form.control}
-            name="category"
-            render={({ field }) => (
-              <FormItem className="col-span-2">
-                <Select onValueChange={field.onChange} value={field.value}>
-                  <FormControl>
-                    <SelectTrigger>
-                      <SelectValue placeholder="Select A Category" />
-                    </SelectTrigger>
-                  </FormControl>
-                  <SelectContent>
-                    <SelectItem value="dispersed">
-                      <FontAwesomeIcon className="pe-5" icon={faMountain} />
-                      Dispersed Campsite
-                    </SelectItem>
-                    <SelectItem value="city">
-                      <FontAwesomeIcon className="pe-5" icon={faCity} />
-                      City Campsite
-                    </SelectItem>
-                    <SelectItem value="established">
-                      <FontAwesomeIcon className="pe-5" icon={faCampground} />
-                      Established Campsite
-                    </SelectItem>
-                  </SelectContent>
-                </Select>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          {/* Pictures */}
-          <FormField
-            name="pics"
-            control={form.control}
-            render={({ field }) => {
-              return (
-                <FormItem className="col-span-2">
-                  <FormLabel>Pictures</FormLabel>
-                  <FormControl>
-                    <Input
-                      defaultValue={field.value}
-                      placeholder="Location Name"
-                      type="file"
-                      multiple
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          ></FormField>
-          {/* description */}
-          <FormField
-            name="description"
-            control={form.control}
-            render={({ field }) => {
-              return (
-                <FormItem className="col-span-2">
-                  <FormLabel>description</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder="Tell us about the place"
-                      rows={4}
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          ></FormField>
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
+      <Step1
+        form={form}
+        formSchema={formSchema}
+        handleSubmit={handleSubmit}
+        handleFindOnMap={handleFindOnMap}
+        handleUseLocation={handleUseLocation}
+        gettingLocation={gettingLocation}
+      />
     </div>
   );
 };
---
+++ src/app/views/add-location-steps/Step1.tsx
@@ -0,0 +1,197 @@
+import React from "react";
+import * as z from "zod";
+import { useForm, UseFormReturn } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
+// Define the schema and its inferred type
+const formSchema = z.object({
+  locationName: z.string().nonempty("Location Name is required"),
+  longitude: z.number(),
+  latitude: z.number(),
+  category: z.string(),
+  description: z.string(),
+  pics: z.string().optional(),
+});
+
+type FormData = z.infer<typeof formSchema>;
+
+interface Step1Props {
+  form: UseFormReturn<FormData>; // Type should be inferred from the schema
+  handleSubmit: (data: FormData) => Promise<void>;
+  handleFindOnMap: () => void;
+  handleUseLocation: () => void;
+  gettingLocation: boolean;
+  formSchema: any;
+}
+
+const Step1: React.FC<Step1Props> = ({
+  form,
+  handleSubmit,
+  handleFindOnMap,
+  handleUseLocation,
+  gettingLocation,
+}) => {
+  return (
+    <Form {...form}>
+      <form
+        className="grid gap-5 grid-cols-2 p-5"
+        onSubmit={form.handleSubmit(handleSubmit)}
+      >
+        {/* location Name */}
+        <FormField
+          name="locationName"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem className="col-span-2">
+              <FormLabel>Location Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Location Name" type="text" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* longitude */}
+        <FormField
+          name="longitude"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem className="col-span-1">
+              <FormLabel>Lng</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="lng"
+                  type="text"
+                  {...field}
+                  onChange={(event) => field.onChange(+event.target.value)}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* latitude */}
+        <FormField
+          name="latitude"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem className="col-span-1">
+              <FormLabel>Lat</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="lat"
+                  type="text"
+                  {...field}
+                  onChange={(event) => field.onChange(+event.target.value)}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* find on map */}
+        <Button type="button" onClick={handleFindOnMap} className="col-span-2">
+          Find Location on Map
+          {/* Add FontAwesome icon here */}
+        </Button>
+        {/* use current location */}
+        <Button
+          type="button"
+          onClick={handleUseLocation}
+          className="col-span-2"
+        >
+          Use Current Location
+          {/* Add FontAwesome icon here */}
+        </Button>
+        {/* category */}
+        <FormField
+          name="category"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem className="col-span-2">
+              <Select onValueChange={field.onChange} value={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select A Category" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="dispersed">
+                    {/* Add FontAwesome icon here */}
+                    Dispersed Campsite
+                  </SelectItem>
+                  <SelectItem value="city">
+                    {/* Add FontAwesome icon here */}
+                    City Campsite
+                  </SelectItem>
+                  <SelectItem value="established">
+                    {/* Add FontAwesome icon here */}
+                    Established Campsite
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* Pictures */}
+        <FormField
+          name="pics"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem className="col-span-2">
+              <FormLabel>Pictures</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Upload pictures"
+                  type="file"
+                  multiple
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* description */}
+        <FormField
+          name="description"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem className="col-span-2">
+              <FormLabel>Description</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Tell us about the place"
+                  rows={4}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default Step1;
